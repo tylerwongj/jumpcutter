1c926151675bbdd56823a8cc5b77259c86a85ab0
Tyler Wong <tylerwong.j@gmail.com>
2020-06-25 01:33:28 -0700

    master - add notes.sh

---
+++ notes.sh
@@ -0,0 +1,12 @@
+## jumpcutter - video editor by python code (https://towardsdatascience.com/edit-your-videos-automatically-with-this-python-code-c704cc413a1)
+# cd ~/p/_3RD-PARTY/jumpcutter
+# python jumpcutter.py --input_file "2020-06-24\ 19-05-45.mp4" --sounded_speed 1 --silent_speed 999999 --frame_margin 2
+# python jumpcutter.py --url https://www.youtube.com/watch?v=okLRV3kwwRk --sounded_speed 1 --silent_speed 999999 --frame_margin 2
+
+
+## How to setup:
+# Get Python 3 (and pip) installed for your computer - probably with pyenv
+# Install requirements in requirements.txt (there's some cmd you can run, I forget...)
+# brew install ffmpeg
+# Place video file in this folder (or use a YOUTUBE link)
+# Run the CMD above
